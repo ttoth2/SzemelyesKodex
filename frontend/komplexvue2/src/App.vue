4e3f0673c4235d8104efbd33039cd9988da73a03
ttoth2 <118882072+ttoth2@users.noreply.github.com>
2025-10-17 10:06:06 +0200

    ora

---
+++ frontend/komplexvue2/src/App.vue
@@ -0,0 +1,53 @@
+<script>
+//import HelloWorld from './components/HelloWorld.vue'
+export default {
+  data() {
+    return {
+      message: 'Hello VÚ !'
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>{{ message }}</h1>
+  <h1>ÉTELEK</h1>
+  <div id="wrapper">
+    <food-item 
+    food-name="Alma"
+    food-desc="A külömböző Almák a fákon teremnek"/>
+    <food-item 
+    food-name="Pizza"
+    food-desc="A Pizza a kománál készül"
+    :is-favourite="true"/>
+    <food-item 
+    food-name="Rizs"
+    food-desc="A Rizset Soma termelte"/>
+  </div>
+  
+
+</template>
+
+<style>
+  #wrapper> div{
+    border:dashed 1px black;
+    display: inline-block;
+    flex-basis: 120px;
+    margin: 10px;
+    padding: 10px;
+    background-color: rgb(43, 255, 43);
+    border-radius: 40%;
+    -webkit-user-select: none; /* Safari */
+    -ms-user-select: none; /* IE 10 and IE 11 */
+    user-select: none; /* Standard syntax */
+  }
+
+  #wrapper> div:hover{
+    cursor: pointer
+  }
+  #wrapper{
+    display: flex;
+    flex-wrap: wrap;
+  }
+
+</style>
---
+++ frontend/komplexvue2/src/components/FoodItem.vue
@@ -0,0 +1,66 @@
+<template>
+    <div @click="FAV" >
+        <h2>{{ foodName }}
+        <img src="../img_quality.svg" v-show="Fav" title="Komákoskép">
+        </h2>
+        <p>{{ foodDesc }}</p>
+        
+        <!-- <p id="red">klikkelések:{{  clicks}}</p> -->
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        foodName: {
+            type: String,
+            required: true,
+        },
+        foodDesc: {
+            type: String,
+            required: false,
+            default: 'Nincs leiras',
+            validator: function (value) {
+                if (20 < value && value.lenght < 50) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        } ,
+        isFavourite: {
+            type: Boolean,
+            required: false,
+            default: false,
+        },
+    },
+    data() {
+        return {
+            Fav: this.isFavourite
+        }
+    },
+    
+
+    /*data() {
+        return {
+            name: 'Alma',
+            message: 'szeretem az amlát',
+            clicks: 0
+        }
+    },*/
+    methods: {
+        FAV() {
+            this.Fav = !this.Fav;
+        }
+    }
+}
+</script>
+<style>
+#red{
+    color: red;
+    font-weight: bold;
+}
+img{
+    height: 1.5em;
+    float: right;
+}
+</style>
